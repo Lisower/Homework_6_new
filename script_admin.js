4c307953ebf4b23797d48ed43e7e6d294616e898
Lisower <144545336+Lisower@users.noreply.github.com>
2024-05-06 15:28:57 +0300

    Update script_admin.js

--- script_admin.js
+++ script_admin.js
@@ -1,12 +1,12 @@
 const Button_Stats = document.getElementById('Button_Stats');
 const Button_Exit = document.getElementById('Exit');
 const Button_Change = document.getElementById('Button_Change');
-const Popup_Stats = document.getElementById('Popup_Stats');
+const Popup = document.getElementById('Popup');
 const Stats = document.getElementById('Stats');
 const Form = document.getElementById('Form');
 
 Button_Stats.addEventListener('click', () => {
-    Popup_Stats.style.display = 'block';
+    Popup.style.display = 'block';
 });
 
 Button_Exit.addEventListener('click', () => {
