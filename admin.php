60bbacba9279847b6237271ffb7e73bda2835764
Lisower <144545336+Lisower@users.noreply.github.com>
2024-05-06 14:55:40 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -12,7 +12,7 @@
 if (empty($_SERVER['PHP_AUTH_USER']) ||
     empty($_SERVER['PHP_AUTH_PW']) ||
     $_SERVER['PHP_AUTH_USER'] != 'admin' ||
-    md5($_SERVER['PHP_AUTH_PW']) != md5('123')) {
+    md5($_SERVER['PHP_AUTH_PW']) != md5('admin_post_admin')) {
   header('HTTP/1.1 401 Unanthorized');
   header('WWW-Authenticate: Basic realm="My site"');
   print('<h1>401 Требуется авторизация</h1>');
