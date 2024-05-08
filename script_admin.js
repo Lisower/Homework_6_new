d83a4dde8b16c201ba5cfb6081808a92c44e1855
Lisower <144545336+Lisower@users.noreply.github.com>
2024-05-08 19:31:22 +0300

    Update script_admin.js

--- script_admin.js
+++ script_admin.js
@@ -1,14 +1,9 @@
 const Button_Stats = document.getElementById('Button_Stats');
-const Button_Exit = document.getElementById('Exit');
 const Button_Change = document.getElementById('Button_Change');
 const Popup = document.getElementById('Popup');
 const Stats = document.getElementById('Stats');
 const Form = document.getElementById('Form');
 
-Button_Exit.addEventListener('click', () => {
-    window.location.href = 'logout_admin.php';
-});
-/*
 Button_Stats.addEventListener('click', () => {
     Popup.style.display = 'block';
 });
@@ -20,4 +15,3 @@ Button_Change.addEventListener('click', () => {
 window.addEventListener('popstate', () => {
     Popup.style.display = 'none';
 });
-*/
