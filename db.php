8515d9b7b5e0cd7cfd2a9d0ea53b326d6ab74b8b
Lisower <144545336+Lisower@users.noreply.github.com>
2024-05-08 18:41:08 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,6 @@
 <?php
+include('credentials.php')
+
 function db_PDO(){
   $db = new PDO('mysql:host=localhost;dbname=u67447', $GLOBALS['user'], $GLOBALS['pass'],
     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
